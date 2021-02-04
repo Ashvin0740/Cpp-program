2c2710737132cc4aa112457476416fb5b66623a7
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-02-04 18:19:25 +0530

    comment
    comment in c++

---
+++ comment.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    // single line comment
+   cout<< "single line comment";
+       /* multi 
+       line 
+        commen
+        */
+   cout<< " multi line comment";
+   return 0;
+}
