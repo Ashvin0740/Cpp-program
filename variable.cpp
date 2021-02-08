cd566a54cfbb213e647e6da92237cf8fdce71304
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-02-08 16:57:23 +0530

    variable
    variable in c++

---
+++ variable.cpp
@@ -0,0 +1,20 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+ 
+    int a = 14;
+    int b = 15;
+    // int a=14, b=15;
+    float pi=3.14;
+    char c ='d';
+    bool is_true = false;
+    // cout<<is_true;
+    cout<<"Here the value of a is "<<a<<".\nThe value of b is "<< b;
+    cout<<"\nThe value of pi is: "<<pi;
+    cout<<"\nThe value of c is: "<<c;
+    cout<< "\n the value of bool is: "<<is_true;
+
+    return 0;
+}
