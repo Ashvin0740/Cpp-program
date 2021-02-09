77673dac97b0983719460932693052f0eff0647d
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-02-09 09:45:25 +0530

    for loop
    for loop

---
+++ ForLoop.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+
+    /*For loop in C++*/
+    // int i=1;
+    // cout<<i;
+    // i++;
+
+    // Syntax for for loop
+    // for(initialization; condition; updation)
+    // {
+    //     loop body(C++ code);
+    // }
+
+    for (int i = 1; i <= 40; i++)
+    {
+        /* code */
+        cout<<i<<endl;
+    }
+
+    // Example of infinite for loop
+    // for (int i = 1; 34 <= 40; i++)
+    // {
+    //     /* code */
+    //     cout<<i<<endl;
+    // }
+    return 0;
+}
