f8f17a020b7ebb24fec9269108fda009b469bc56
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-02-09 09:49:22 +0530

    do while loop
    do while loop

---
+++ DoWhileLoop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{ 
+    /* do While loop in C++*/
+        // Syntax:
+        // do
+        // {
+        //     C++ statements;
+        // }while(condition);
+        
+//  Printing 1 to 100 using while loop
+    int i=1;
+    do{
+        cout<<i<<endl;
+        i++;
+    }while(i<=100);
+
+    return 0;
+}
