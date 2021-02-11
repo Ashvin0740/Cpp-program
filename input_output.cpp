5a5af0ace18817421769fc3a277b799e0888aaf1
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-02-11 18:25:38 +0530

    input output
    input output

---
+++ input_output.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+ using namespace std;
+
+ int main()
+ {
+     int num1,num2;
+     cout << "inter value num1: \n";/* '<<' is called Insertion operator */
+     cin >> num1;/* '>>' is called Extraction operator */
+
+     cout << "inter value num2: \n"; /* '<<' is called Insertion operator */
+     cin >> num2;/* '>>' is called Extraction operator */
+
+     cout << "sum of two number is :"<< num1 + num2;
+
+ }
+
+
