4a160c15fc3226f0dd4a90b7f4d8bf07b6a7bdaf
Ashvin0740 <67723298+Ashvin0740@users.noreply.github.com>
2021-02-18 17:48:21 +0530

    factorial in c++
    factorial in c++

---
+++ factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n<2){
+        return 1;
+    }else{
+        return n * factorial(n-1);
+    }
+}
+int main() {
+     int num;
+    cout<<"Enter a Number :" << endl;
+    cin>>num;
+    cout<<"The factorial of " << num << " is "<< factorial(num) <<endl;
+
+     return 0;
+}
